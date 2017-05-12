25cd4ec3ef0e00b7ec776892e2cbef2539afe976
Nils Knappmeier <github@knappi.org>
2017-05-12 10:19:13 +0200

    BREAKING: Partial "base/html-head.hbs" is not public API anymore
    * The partial "html-head-extra" can now be used to add extra content to
      the "head" tag.
    * Added note to "html-head.hbs" about the change
    * Added tests for all extension-point-partials

--- test/extension-points/spec.js
+++ test/extension-points/spec.js
@@ -14,9 +14,13 @@ var expect = chai.expect
 var path = require('path')
 var tester = require('bootprint-unit-testing')
 
-describe('Checking extension partials ', function () {
+describe.only('Extension points: bootprint-base', function () {
   this.timeout(10000)
 
+  /**
+   * Load bootprint-base and merge custom partials for this tests
+   * @param customize
+   */
   function module (customize) {
     return customize
       .load(require('../..'))
@@ -27,13 +31,39 @@ describe('Checking extension partials ', function () {
       })
   }
 
-  var bptest = tester(module, __dirname, {})
+  var bptest = tester(module, __dirname, {name: 'extension-points-spec'})
 
   // Run bootprint. The parsed "index.html"-file (cheerio) is then available
   // under "bptest.$"
   before(bptest.run)
 
   it('should include a javascript-library', function () {
-    expect(bptest.$('script[src="myjslib.js"]').length).to.equal(1)
+    expect(bptest.$('body *').last().toString())
+      .to.equal('<script src="extension-points-spec.js" type="text/javascript"></script>')
+  })
+
+  it('should include the partial "base/header" in front of the body container', function () {
+    expect(bptest.$('div.container').prev().toString())
+      .to.equal('<header>header extension-points-spec</header>')
+  })
+
+  it('should include the partial "base/body" inside the body container', function () {
+    expect(bptest.$('div.container').html().trim())
+      .to.equal('body extension-points-spec')
+  })
+
+  it('should include the partial "base/footer" after the body container', function () {
+    expect(bptest.$('div.container').next().toString())
+      .to.equal('<footer>footer extension-points-spec</footer>')
+  })
+
+  it('should include the partial "base/html-head-extra" at the end of the head-tag', function () {
+    expect(bptest.$('head *').last().toString())
+      .to.equal('<html-head-extra>html-head-extra extension-points-spec</html-head-extra>')
+  })
+
+  it('should include the partial "base/title" inside the title tag', function () {
+    expect(bptest.$('head > title').html().trim())
+      .to.equal('title extension-points-spec')
   })
 })
